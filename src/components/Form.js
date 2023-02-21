47d83ecc3bea205156939f75a839fc5789f6a6c8
StasMarfenko <62810586+StasMarfenko@users.noreply.github.com>
2023-02-21 15:43:03 +0600

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,7 +15,7 @@ import React from "react";
 const Form = props => (
     <form onSubmit={props.weatherMethod} className="py-10 text-center xl:text-left xl:pl-10">
     <input type="text" name="city" placeholder="Город" className="w-44 outline-none p-3 rounded mb-5 mx-5 bg-slate-200 focus:bg-white hover:bg-white transition " />
-    <button className="w-44 bg-yellow-300 hover:bg-yellow-400 transition p-3 rounded text-gray-700">Узнать погоду</button>
+    <button className="mx-5 w-44 bg-yellow-300 hover:bg-yellow-400 transition p-3 rounded text-gray-700">Узнать погоду</button>
 </form>
 )
 
